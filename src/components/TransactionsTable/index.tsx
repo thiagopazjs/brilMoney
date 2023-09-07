caa0165919fdd43cb5c44fbae9e8ba3cc4763608
Thiago Paz <tpazdev@gmail.com>
2023-09-07 17:43:12 -0300

    feat: add mirage js

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+
+    useEffect (() => {
+
+        fetch ('http://localhost:5173/api/transactions').then(response => response.json()).then(data => console.log(data))
+
+    })
     return (
         <Container>
             <table>
