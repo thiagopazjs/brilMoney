2e190b7895c5b651a3fe1f70c959d8447dd2dd97
Thiago Paz <tpazdev@gmail.com>
2023-09-11 11:56:05 -0300

    feat: add react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard"
 import { Header } from "./components/Header"
 import { GlobalStyle } from "./styles/global"
+import { useState } from 'react';
 
+
+
+
+Modal.setAppElement('#root')
 
 
 export function App() {
+
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+
+  }
 
 
   return (
@@ -12,7 +31,14 @@ export function App() {
     <>
 
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>cadastrar transação</h2>
+      </Modal>
       <Dashboard />
 
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import igniteImg from '../../assets/Ignite simbol.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
 
     return (
 
         <Container>
             <Content>
-               <img src={igniteImg}  alt="bril-money"  /> <span>Bril Money</span> 
-                <button className='btn' type="button" >
-                Nova transação
+                <img src={igniteImg} alt="bril-money" /> <span>Bril Money</span>
+                <button className='btn' type="button" onClick={onOpenNewTransactionModal}>
+                    Nova transação
                 </button>
+
+
             </Content>
         </Container>
     )
